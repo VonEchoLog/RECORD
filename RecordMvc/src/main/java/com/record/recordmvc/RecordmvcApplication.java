3a4ed209cacea201348a7cf55e14613c6f165f00
VonEchoLog <745476416@qq.com>
2021-12-26 03:13:00 +0800

    recordMvc commit 2021-12-26 FBY

---
+++ RecordMvc/src/main/java/com/record/recordmvc/RecordmvcApplication.java
@@ -0,0 +1,13 @@
+package com.record.recordmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordmvcApplication.class, args);
+    }
+
+}
---
+++ RecordMvc/src/test/java/com/record/recordmvc/RecordmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record.recordmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
